9021806e2f5ed3c6b80aa2c677831e1b09b811dd
HarikaChalla03 <107341665+HarikaChalla03@users.noreply.github.com>
2022-06-30 02:14:38 +0530

    Add files via upload
    First Assignment on Department Project

---
+++ com/greatlearning/parentclass/SuperDepartment.java
@@ -0,0 +1,26 @@
+package com.greatlearning.parentclass ;
+
+public class SuperDepartment
+{
+	
+		public String departmentName() 
+		{
+			return " Welcome to Super Department " ;
+		}
+		
+		public String getTodaysWork()
+		{
+			return " No Work as of now " ;
+	    }
+
+		public String getWorkDeadline()
+		{
+			return " Nill " ;
+		}
+		
+		public String isTodayAHoliday()
+		{
+			return " Today is not a Holiday " ;
+		}
+	   
+}
